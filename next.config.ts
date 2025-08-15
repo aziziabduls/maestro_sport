163fe3ee96545796044c046132f1df6ca1e0fbb2
abdul.azizi <abdul.azizi@primecash.co.id>
2025-08-15 18:02:08 +0700

    Menambahkan konfigurasi baru pada next.config.ts, termasuk pengaturan output, trailingSlash, basePath, dan pengaturan gambar yang tidak teroptimasi.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: "export",
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === "production" ? "/maestro_sport" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
